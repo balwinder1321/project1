1837acb1c9e39f2464810de0517ebfcf0c647357
balwinder1321 <balwindersinghsardar1@gmail.com>
2025-08-19 14:44:04 +0530

    sample1.java with content balwinder1321

---
+++ jenkinproject/sample1.java
@@ -0,0 +1,35 @@
+pipeline {
+agent any
+stages {
+stage('Checkout Code') {
+steps {
+git branch: 'main', url: 'https://github.com/deepak574/project.git'
+}
+}
+stage('Build') {
+steps {
+bat 'javac Sample.java'
+}
+}
+stage('Test') {
+steps {
+bat 'java Sample'
+}
+}
+stage('Deploy') {
+steps {
+echo 'Deploying application...'
+// Add your deployment commands here (like copying files to a server)
+}
+}
+post
+{
+success
+{
+mail to:’gujjuladeepak@gmail.com’,
+subject: “Build success”,
+Body: ‘Your build was successfull’
+}
+}
+}
+}
